5bd60466a815b76923e16780718771ad47982280
BaNanoIT <jealsanchezto@ittepic.edu.mx>
2024-10-25 13:09:35 -0700

    Estamos ganando la batalla contra CSS

--- logica.js
+++ logica.js
@@ -1,19 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const secciones = document.querySelectorAll("main section");
-  const enlaces = document.querySelectorAll(".indice a");
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Documentacion TaskProject', 'Flare Dev. Team', 'Instituto Tecnologico de Tepic', 'Desarrollo de Servicios Web'], 'text',['#D4AC0D','#17A589','#00796B', '#00274D']);
 
-  window.addEventListener("scroll", function() {
-    let fromTop = window.scrollY + 10;
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('consola');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-    secciones.forEach((seccion, index) => {
-      const enlace = enlaces[index];
-      const offsetTop = seccion.offsetTop;
-      const offsetBottom = seccion.offsetTop + seccion.offsetHeight;
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'consola-guion-bajo hidden'
+      visible = false;
 
-      if (fromTop >= offsetTop && fromTop < offsetBottom) {
-        enlaces.forEach(a => a.classList.remove("active"));
-        enlace.classList.add("active");
-      }
-    });
-  });
-});
+    } else {
+      con.className = 'consola-guion-bajo'
+
+      visible = true;
+    }
+  }, 400)
+}
